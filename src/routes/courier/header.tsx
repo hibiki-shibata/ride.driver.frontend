8486f9dfd9c3226799290222b32c160f5990db81
hibiki-shibata <hibiki.shibata@wolt.com>
2025-12-21 04:32:43 +0900

    Edit: href to /home courier/header.tsx

--- src/routes/courier/header.tsx
+++ src/routes/courier/header.tsx
@@ -2,7 +2,7 @@ function header() {
     return (
         <div className="bg-black text-white flex justify-between">
             <h1 className="pt-2 pl-10 text-5xl">~ Amazones ~</h1>
-            <a href="/home" className="pr-10 hover:underline p-3 m-3">
+            <a href="/ride.driver.frontend/home" className="pr-10 hover:underline p-3 m-3">
                 Explore as a Customer
             </a>
         </div>
