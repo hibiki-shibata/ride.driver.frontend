39cd49655c8de775a0aa8abad305a734530a8b73
hibiki-shibata <hibiki.shibata@wolt.com>
2025-12-26 03:31:25 +0900

    Edit: rm route level header but compile into one in component

--- src/components/buttons/couriers/courierSignupButton.tsx
+++ src/components/buttons/couriers/courierSignupButton.tsx
@@ -1,7 +1,7 @@
 function courierSignupButton() {
     return (
         <a href="/ride.driver.frontend/courier/apply">
-            <button className="p-1 mt-15 bg-sky-600 rounded-lg p-3 m-3 hover:bg-sky-700" >
+            <button className="p-1 mt-15 bg-sky-600 rounded-lg p-3 m-3 hover:bg-sky-700">
                 Sign up now
             </button>
         </a>
