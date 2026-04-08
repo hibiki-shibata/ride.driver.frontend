fb2b4d8f0fd53241719b120de727702cf4c2f4c2
hibiki-shibata <hibiki.shibata@wolt.com>
2026-04-08 16:29:44 -0500

    Edit: href foter

--- src/shared/component/footer.tsx
+++ src/shared/component/footer.tsx
@@ -1,5 +1,6 @@
-import { FRONTEND_URI } from "../constant/apiUri"
 import { COURIER_ROUTE } from "../constant/hrefRoute"
+
+const profilePageUri = "https://hibiki-shibata.github.io/"
 
 function Footer() {
     return (
@@ -7,8 +8,8 @@ function Footer() {
             <div className="bg-black text-white p-15 flex justify-center">
                 <ul className="flex flex-wrap items-center mt-1 text-sm font-medium text-gray-500 space-x-4">
                     <li><a className="hover:underline" href={COURIER_ROUTE.HOME}>Courier page</a></li>
-                    <li><a href={FRONTEND_URI} className="hover:underline">Contact: hibiki.shibata@wolt.com</a></li>
-                    <li><a href={FRONTEND_URI} className="hover:underline">Service owner: @Hibiki</a></li>
+                    <li><a href={profilePageUri} className="hover:underline">Contact: hibiki.shibata@wolt.com</a></li>
+                    <li><a href={profilePageUri} className="hover:underline">Service owner: @Hibiki</a></li>
                 </ul>
             </div>
         </>
