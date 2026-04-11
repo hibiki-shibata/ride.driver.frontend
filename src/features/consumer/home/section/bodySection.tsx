f14f2c3a8a53710a5fb2cd9a25cf7f4dd19b5fa6
hibiki-shibata <hibiki.shibata@wolt.com>
2026-04-10 22:51:05 -0300

    Mv: toExploreCpBtn

--- src/features/consumer/home/section/bodySection.tsx
+++ src/features/consumer/home/section/bodySection.tsx
@@ -1,15 +1,13 @@
 import ServiceDescription from './serviceDescription'
 import Headline from '../component/headline'
-import ExploreCpButton from '../../explore/component/exploreCpButton'
+import ToExploreCpBtn from '../component/toExploreCpBtn'
 
 function ConsumerHomeBodySection() {
     return (
         <div className='bg-gradient-to-b from-gray-900 to-blue-950 text-white pt-20 pb-20 px-2 md:px-15'>
             <Headline />
-            <ExploreCpButton />
-            <div>
-                <ServiceDescription />
-            </div>
+            <ToExploreCpBtn />
+            <ServiceDescription />
         </div>
     )
 }
