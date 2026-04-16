73bfb856d1185d10ccd5289f1b939f30358ea957
hibiki-shibata <hibiki.shibata@wolt.com>
2026-04-16 06:43:32 -0300

    Rm: test

--- src/feature/consumer/menuAndCheckout/hook/useMerchantMenu.ts
+++ src/feature/consumer/menuAndCheckout/hook/useMerchantMenu.ts
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react"
 import { getMenu } from "../api/getMenu"
 import type { MenuItem } from "../type/menuItem"
 
-function testMenuItems(): MenuItem[] { // Remove later
-  const menuItems: MenuItem[] = []
-  for (let i = 0; i < 10; i++) {
-    menuItems.push({
-      id: `${i}`,
-      name: `Menu Item ${i}`,
-      description: `This is menu item ${i}'s description.`,
-      price: 10 + i,
-      enabled: i % 2 === 0,
-    })
-  }
-  return menuItems
-}
+// function testMenuItems(): MenuItem[] { // Remove later
+//   const menuItems: MenuItem[] = []
+//   for (let i = 0; i < 10; i++) {
+//     menuItems.push({
+//       id: `${i}`,
+//       name: `Menu Item ${i}`,
+//       description: `This is menu item ${i}'s description.`,
+//       price: 10 + i,
+//       enabled: i % 2 === 0,
+//     })
+//   }
+//   return menuItems
+// }
 
 type UseMerchantMenuResult = {
   menuItems: MenuItem[]
@@ -30,31 +30,31 @@ export function useMerchantMenu(merchantName: string): UseMerchantMenuResult {
   useEffect(() => {
     let isMounted = true
 
-    setMenuItems(testMenuItems()) // Remove later
+    // setMenuItems(testMenuItems()) // Remove later
 
-    // async function fetchMenu() {
-    //   try {
-    //     const response = await getMenu(merchantName)
+    async function fetchMenu() {
+      try {
+        const response = await getMenu(merchantName)
 
-    //     if (!isMounted) return
+        if (!isMounted) return
 
-    //     setMenuItems(response)
-    //     setError(null)
-    //   } catch (error: unknown) {
-    //     if (!isMounted) return
+        setMenuItems(response)
+        setError(null)
+      } catch (error: unknown) {
+        if (!isMounted) return
 
-    //     setError(
-    //       error instanceof Error
-    //         ? error.message
-    //         : "Failed to load menu."
-    //     )
-    //   } finally {
-    //     if (!isMounted) return
-    //     setIsLoading(false)
-    //   }
-    // }
+        setError(
+          error instanceof Error
+            ? error.message
+            : "Failed to load menu."
+        )
+      } finally {
+        if (!isMounted) return
+        setIsLoading(false)
+      }
+    }
 
-    // fetchMenu()
+    fetchMenu()
 
     return () => {
       isMounted = false
