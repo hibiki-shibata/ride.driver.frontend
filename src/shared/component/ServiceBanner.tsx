fd9c046d6c0c21105db4dd479e524c2feb4adc96
Hibiki Shibata <hibiki@hibiki-shibata.local>
2026-05-11 23:09:41 -0400

    Edit: ServiceBanner animate plus & flex row in small screen

--- src/shared/component/ServiceBanner.tsx
+++ src/shared/component/ServiceBanner.tsx
@@ -1,14 +1,16 @@
 function ServiceBanner() {
     return (
-        <p className="text-white bg-red-700 flex justify-center">
-            🚨 The backend instance is not running by default for cost reasons - 
-            <a href="https://drive.google.com/file/d/1ko3GkkVkxu2W7JOJMOL2H4MTNhOH539g/view" target="_blank" className="text-blue-400 underline">
-                Check demo video
-            </a>
-             OR
-            <a href="https://hibiki-shibata.github.io/#contact-card" target="_blank" className="text-blue-400 underline">
-                reach out to Hibiki
-            </a>
+        <p className=" py-1 text-white bg-red-900 md:flex text-center md:flex-row md:justify-center animate-pulse">
+            🚨 The backend instance is not running by default for cost reasons -
+            <div>
+                <a href="https://drive.google.com/file/d/1ko3GkkVkxu2W7JOJMOL2H4MTNhOH539g/view" target="_blank" className="text-blue-400 underline">
+                    Check demo video
+                </a>
+                OR
+                <a href="https://hibiki-shibata.github.io/#contact-card" target="_blank" className="text-blue-400 underline">
+                    reach out to Hibiki
+                </a>
+            </div>
         </p>
     )
 }
